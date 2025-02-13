f6ee10397833de4be6dc10005b11900e81b6798c
liqiujiang <1554994006@qq.com>
2025-02-13 19:00:55 +0800

    refactor: Simplify BNB balance retrieval with async/await

--- src/api/getBnbBalance.ts
+++ src/api/getBnbBalance.ts
@@ -1,7 +1,7 @@
 import request from "./request";
 
 export default async function getBnbBalance(wallet: string) {
-  return request(
+  const res = await request(
     "https://bnb-mainnet.g.alchemy.com/v2/gW2ar3jVYkL5ub53Z7Z-XwWVuALzTRXJ",
     {
       method: "POST",
@@ -15,5 +15,7 @@ export default async function getBnbBalance(wallet: string) {
         id: 1,
       }),
     }
-  ).then((res) => Number((BigInt(res.result) / BigInt(10 ** 18)).toString()));
+  );
+
+  return Number((BigInt(res.result) / BigInt(10 ** 18)).toString());
 }
